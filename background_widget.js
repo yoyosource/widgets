89ce3c5c4088c93bab68f4b160923ffd5fd55225
yoyosource <46222147+yoyosource@users.noreply.github.com>
2022-09-04 15:04:10 +0200

    Fix spelling

--- background_widget.js
+++ background_widget.js
@@ -20,7 +20,7 @@ if (!config.runsInWidget) {
 
   let keys = type.split("-")
   crop.y = sizes[keys[0]]
-  crop.x = sites[keys[1]]
+  crop.x = sizes[keys[1]]
 
   let image = cropImage(args.images[0], new Rect(crop.x,crop.y,crop.w,crop.h))
   fm.writeImage(fm.joinPath(fm.documentsDirectory(), type + ".png"), image)
