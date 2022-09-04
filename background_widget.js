115103ccccf66d4663e206c9d60cfa0c8a32d4f9
yoyosource <46222147+yoyosource@users.noreply.github.com>
2022-09-04 15:27:39 +0200

    Update generation

--- background_widget.js
+++ background_widget.js
@@ -5,18 +5,19 @@ var type = "top-left"
 const fm = FileManager.local()
 
 if (!config.runsInWidget) {
+  // 1792 * 828
   const sizes = {
-    size: 310,
-    left: 46,
-    right: 394,
-    top: 142,
-    middle: 522,
-    bottom: 902
+    small:  338,
+    left:   54,
+    right:  436,
+    top:    160,
+    middle: 580,
+    bottom: 1000
   }
 
   let crop = { w: "", h: "", x: "", y: "" }
-  crop.w = sizes.size
-  crop.h = sizes.size
+  crop.w = sizes.small
+  crop.h = sizes.small
 
   let keys = type.split("-")
   crop.y = sizes[keys[0]]
@@ -33,7 +34,7 @@ function cropImage(img,rect) {
   let draw = new DrawContext()
   draw.size = new Size(rect.width, rect.height)
   
-  draw.drawImageAtPoint(img,new Point(-rect.x - img.width / 2, -rect.y - img.height / 2))  
+  draw.drawImageAtPoint(img,new Point(-rect.x - (img.width - 828) / 2, -rect.y - (img.height - 1792) / 2))  
   return draw.getImage()
 }
 
