6554e83fabbb437df14d641427ace9c971f7ac31
yoyosource <46222147+yoyosource@users.noreply.github.com>
2022-09-04 14:57:02 +0200

    Add image croping

--- background_widget.js
+++ background_widget.js
@@ -4,19 +4,40 @@ var type = "top-left"
 // ---- CODE ----
 const fm = FileManager.local()
 
-const sizes = {
-  size: 310,
-  left: 46,
-  right: 394,
-  top: 142,
-  middle: 522,
-  bottom: 902
-}
-
 if (config.runsInActionExtension) {
-  // TODO: Create image
+  const sizes = {
+    size: 310,
+    left: 46,
+    right: 394,
+    top: 142,
+    middle: 522,
+    bottom: 902
+  }
+
+  let crop = { w: "", h: "", x: "", y: "" }
+  crop.w = sizes.size
+  crop.h = sizes.size
+
+  let keys = type.split("-")
+  crop.y = sizes[keys[0]]
+  crop.x = sites[keys[1]]
+
+  let image = cropImage(images[0], new Rect(crop.x,crop.y,crop.w,crop.h))
+  fm.writeImage(fm.joinPath(fm.documentsDirectory(), type + ".png"), image)
   return
 }
 
+// Crop an image into the specified rect.
+function cropImage(img,rect) {
+   
+  let draw = new DrawContext()
+  draw.size = new Size(rect.width, rect.height)
+  
+  draw.drawImageAtPoint(img,new Point(-rect.x, -rect.y))  
+  return draw.getImage()
+}
+
+// ---- WidgetCode ----
 let widget = new ListWidget()
 widget.backgroundImage = fm.readImage(fm.joinPath(fm.documentsDirectory(), type + ".png"))
+Script.setWidget(widget)
